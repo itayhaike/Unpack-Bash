bbf7097433a26c58cbf0d40437f1bbe0baaf7576
Itay Haike <81685066+itayhaike@users.noreply.github.com>
2022-10-20 14:37:33 +0300

    Create Unpack.sh

---
+++ Unpack.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+FILE=(*.{zip,bz2,cmpr,gz})
+
+COUNT=0
+
+function unpack (){
+
+ if [ -f $1 ] ; then
+         case $1 in
+             *.tar.bz2)   tar xjf $1     
+             		  ((COUNT++))
+             		  ;;
+             *.tar.gz)    tar xzf $1     
+             		   ((COUNT++))
+             		   ;;
+             *.bz2)       bunzip2 $1     
+             	          ((COUNT++))
+             		   ;;
+             *.rar)       rar x $1       
+             		   ((COUNT++))
+             		   ;;
+             *.gz)        gunzip $1      
+             		   ((COUNT++))
+             		   ;;
+             *.tar)       tar xf $1
+                          ((COUNT++))
+                          ;;
+             *.tbz2)      tar xjf $1     
+             		   ((COUNT++))
+             		   ;;
+             *.tgz)       tar xzf $1     
+             		   ((COUNT++))
+             		   ;;
+             *.zip)       unzip $1       
+             	           ((COUNT++))
+             	           ;;
+             *.Z)         uncompress $1  
+             	           ((COUNT++))
+             	           ;;
+             *.7z)        7z x $1    
+             		   ((COUNT++))
+             		   ;;
+             *)           echo "$1 is not an archive file. "
+                  ;;
+         esac
+     else
+         echo "!!!Error $1 is not a vaild file!!!"
+     fi
+}
+
+function repeat (){
+
+  if [ -d ]; 
+	
+	then 
+	
+	 for d in */; do
+  	  FOLDER="$d" 
+  	  cd $FOLDER
+  
+
+  	  for filename in *.{zip,bz2,cmpr,gz} ; do
+  	  unpack $filename
+  	  done
+	 done
+	 
+	fi
+	
+	
+	 echo " $COUNT -  archives decompressed "
+}
+
+
+unpack "$FILE"
+
+repeat
+
